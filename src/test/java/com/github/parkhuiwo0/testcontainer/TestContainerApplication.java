0eeec979f613c73bfa76afc7e6dd9017070780ef
parkhuiwo0 <huiwo0@naver.com>
2023-03-18 19:40:11 +0900

    테스트를 위한 애플리케이션 컨텍스트 실행 클래스를 정의

---
+++ src/test/java/com/github/parkhuiwo0/testcontainer/TestContainerApplication.java
@@ -0,0 +1,11 @@
+package com.github.parkhuiwo0.testcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestContainerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestContainerApplication.class, args);
+    }
+}
