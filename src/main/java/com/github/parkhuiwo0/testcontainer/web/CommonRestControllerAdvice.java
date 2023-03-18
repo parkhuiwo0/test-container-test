de222c50191cd6ae79851d119f8a4ae78130bbdc
parkhuiwo0 <huiwo0@naver.com>
2023-03-18 19:15:26 +0900

    잘못된 어노테이션를 제거한다

--- src/main/java/com/github/parkhuiwo0/testcontainer/web/CommonRestControllerAdvice.java
+++ src/main/java/com/github/parkhuiwo0/testcontainer/web/CommonRestControllerAdvice.java
@@ -5,12 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.persistence.Version;
-
 @RestControllerAdvice(basePackageClasses = CommonRestControllerAdvice.class)
 class CommonRestControllerAdvice {
 
-    @Version
     @ExceptionHandler(Exception.class)
     ResponseEntity<?> handle(Exception ex) {
         return new ResponseEntity<>(
